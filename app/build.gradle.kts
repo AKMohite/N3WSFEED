0a1a5f50ee879618298883293f10495a7b6b6314
Ashish Mohite <ashish.mohite@ext.saint-gobain.com>
2021-08-07 10:57:06 +0530

    gradle buildsrc configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins{
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdkVersion(Configs.compileSDKVersion)
+
+    defaultConfig {
+        applicationId(Configs.applicationID)
+        minSdkVersion(Configs.minSDKVersion)
+        targetSdkVersion(Configs.targetSDKVersion)
+        versionCode = Configs.versionCode
+        versionName = Configs.versionName
+
+        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles (
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures.viewBinding = true
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Library.kotlinSDK)
+    implementation(Library.appCompat)
+    implementation(Library.coreKtx)
+    implementation(Library.constraintLayout)
+
+//    Hilt
+    implementation(Library.hiltAndroid)
+    kapt(Library.hiltCompiler)
+
+    // ViewModel dependencies
+    implementation(Library.lifecycleViewModel)
+
+    // jetpack navigation component
+    implementation(Library.navigationFragment)
+    implementation(Library.navigationUI)
+    implementation(Library.navigationDynamic)
+
+    // Room
+    implementation(Library.roomRuntime)
+    kapt(Library.roomCompiler)
+
+    // livedate
+    implementation(Library.lifecycleLivedata)
+
+    // retrofit and Gson
+    implementation(Library.retrofit)
+    implementation(Library.gsonConverter)
+
+    // coil
+    implementation(Library.coil)
+
+
+//    datastore
+    implementation(Library.dataStore)
+
+    testImplementation(Library.testJunit)
+    androidTestImplementation(Library.testExtJunit)
+    androidTestImplementation(Library.testEspressoCore)
+}
