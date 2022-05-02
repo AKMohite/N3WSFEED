58d3f09d42f9483f44be94bcef81b728775541e5
Ashish Mohite <ashishmohite1996@gmail.com>
2022-05-02 19:52:20 +0530

    WIP data mapping and image laoding

--- app/src/main/java/com/ak/newsfeed/data/local/entity/NewsArticleEntity.kt
+++ app/src/main/java/com/ak/newsfeed/data/local/entity/NewsArticleEntity.kt
@@ -6,5 +6,11 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "article")
 data class NewsArticleEntity(
     @PrimaryKey(autoGenerate = false)
-    val url: String
+    val url: String,
+    val author: String,
+    val content: String?,
+    val description: String,
+    val publishedAt: String,
+    val title: String,
+    val imgUrl: String
 )
