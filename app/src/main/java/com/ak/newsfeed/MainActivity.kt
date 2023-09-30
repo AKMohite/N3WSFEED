29866da4bc001c68752b1348d34d8f5f2afa92c6
Ashish Mohite <ashishmohite1996@gmail.com>
2023-09-30 20:12:50 +0530

    make system bar transparent

--- app/src/main/java/com/ak/newsfeed/MainActivity.kt
+++ app/src/main/java/com/ak/newsfeed/MainActivity.kt
@@ -8,15 +8,18 @@ import androidx.activity.compose.setContent
 import androidx.annotation.ColorInt
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import com.ak.newsfeed.domain.model.NewsArticle
 import com.ak.newsfeed.ui.home.HomeScreen
 import com.ak.newsfeed.ui.theme.NewsFeedTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,6 +29,14 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             NewsFeedTheme {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemUiColor = rememberSystemUiController()
+                SideEffect {
+                    systemUiColor.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
