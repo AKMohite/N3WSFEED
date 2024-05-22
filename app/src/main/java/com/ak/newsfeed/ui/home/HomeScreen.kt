8a7a9e9516a45f70e4f66efec46739c693d6b703
Ashish Mohite <ashishmohite1996@gmail.com>
2024-05-22 10:08:52 +0530

    Extract viewmodel to navgraph

--- app/src/main/java/com/ak/newsfeed/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ak/newsfeed/ui/home/HomeScreen.kt
@@ -20,8 +20,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,17 +31,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import com.ak.newsfeed.R
 import com.ak.newsfeed.domain.model.NewsArticle
 
 @Composable
 fun HomeScreen(
-    onNewsClick: (NewsArticle) -> Unit
+    onNewsClick: (NewsArticle) -> Unit,
+    viewState: HomeViewState
 ) {
-    val viewModel: HomeViewModel = hiltViewModel()
-    val viewState by viewModel.homeState.collectAsState()
     HomeContent(
         viewState = viewState,
         onNewsClick = onNewsClick
--- app/src/main/java/com/ak/newsfeed/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/ak/newsfeed/ui/navigation/NavGraph.kt
@@ -1,13 +1,29 @@
 package com.ak.newsfeed.ui.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ak.newsfeed.domain.model.NewsArticle
 import com.ak.newsfeed.ui.home.HomeScreen
+import com.ak.newsfeed.ui.home.HomeViewModel
+
+
+data class BottomNavigationItem(
+    val icon: ImageVector,
+    val text: String
+)
 
 @Composable
 internal fun NavGraph(
@@ -15,11 +31,21 @@ internal fun NavGraph(
 ) {
 
     val navController = rememberNavController()
+    val bottomNavigationItems = remember {
+        listOf(
+            BottomNavigationItem(icon = Icons.Outlined.Home, text = "Home"),
+            BottomNavigationItem(icon = Icons.Outlined.Search, text = "Search"),
+            BottomNavigationItem(icon = Icons.Outlined.FavoriteBorder, text = "Bookmark"),
+        )
+    }
 
     NavHost(navController = navController, startDestination = ScreenRoute.Home.route) {
         composable(route = ScreenRoute.Home.route) {
             val primaryInt = MaterialTheme.colorScheme.primary.toArgb()
+            val viewModel: HomeViewModel = hiltViewModel()
+            val viewState by viewModel.homeState.collectAsState()
             HomeScreen(
+                viewState = viewState,
                 onNewsClick = { article ->
                     openWebCustomTab(article, primaryInt)
                 }
